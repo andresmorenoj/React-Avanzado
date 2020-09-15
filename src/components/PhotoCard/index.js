5743b560be8970e745c202f92190e3bf02ecac12
= <=>
2020-09-15 11:38:51 -0500

    [Intersetion observer - animation - useState - useEffect]

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,44 @@
-import React from 'react'
-import {ImgWrapper, Img, Button} from './styles'
-import { MdFavoriteBorder } from "react-icons/md"
+import React, { Fragment, useState, useEffect, useRef } from "react";
+import { Article, ImgWrapper, Img, Button } from "./styles";
+import { MdFavoriteBorder } from "react-icons/md";
 
-const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+const DEFAULT_IMAGE =
+  "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
 
-export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const element = useRef(null);
+  const [show, setShow] = useState(false);
+
+  useEffect(
+    function () {
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0];
+        if (isIntersecting) {
+          console.log("Si");
+          setShow(true);
+          observer.disconnect();
+        }
+      });
+      observer.observe(element.current);
+    },
+    [element]
+  );
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} alt=""/>
-        </ImgWrapper>
-      </a>
+    <Article ref={element}>
+      {show && (
+        <Fragment>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} alt="" />
+            </ImgWrapper>
+          </a>
 
-      <Button>
-       <MdFavoriteBorder size='32px'/> {likes} Likes!
-      </Button>
-    </article>
-  )
-}
+          <Button>
+            <MdFavoriteBorder size="32px" /> {likes} Likes!
+          </Button>
+        </Fragment>
+      )}
+    </Article>
+  );
+};
