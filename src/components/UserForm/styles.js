28abdd1bb53ea802f964001c7615c09f36b360e5
= <=>
2020-09-19 01:16:37 -0500

    [ADD] form styles added

---
+++ src/components/UserForm/styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const Form = styled.form`
+  margin: 0 auto;
+  max-width: 90%;
+  padding: 16px 0;
+`
+
+export const Input = styled.input`
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  margin-bottom: 8px;
+  padding: 8px 4px;
+  display: block;
+  width: 100%;
+`
+
+export const Button = styled.button`
+  background: #8d00ff;
+  border-radius: 3px;
+  color: #fff;
+  height: 32px;
+  display: block;
+  width: 100%;
+  text-align: center;
+  border-style: none;
+`
+
+export const Title = styled.h2`
+  text-align: center;
+  font-size: 16px;
+  font-weight: 500;
+  padding: 8px 0;
+`
