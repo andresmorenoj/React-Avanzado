7396597f5b02f10e5feba02e81da12f73df259f2
= <=>
2020-09-20 14:02:09 -0500

    [ADD] PWA: offline support

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -9,7 +9,7 @@ function useCategoriesData () {
 
   useEffect(function () {
     setLoading(true)
-    window.fetch('https://petgram-server.midudev.now.sh/categories')
+    window.fetch('https://petgram-server-opal.vercel.app/categories')
       .then(res => res.json())
       .then(response => {
         setCategories(response)
